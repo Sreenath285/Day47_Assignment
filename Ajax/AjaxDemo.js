b128831e0f32eb3f180b31dc81869fce55bee3c1
Sreenath285 <sreenath.285@gmail.com>
2022-04-18 13:12:09 +0530

    Asynchronous nature of java script

---
+++ Ajax/AjaxDemo.js
@@ -0,0 +1,12 @@
+function showTime(){
+    const date =new Date();
+    return date.getHours+"Hrs:"+date.getMinutes+"Mins:"+date.getSeconds()+"Secs";
+}
+
+function showSessionExpires(){
+    console.log("Activity-B:your session expired at "+showTime());
+}
+
+console.log("Activity-A:Trigerring Activity-B at "+showTime());
+setTimeout(showSessionExpires,5000);
+console.log("Activity-A:Trigerring Activity-B at "+showTime()+"will execute after 5 sec");
